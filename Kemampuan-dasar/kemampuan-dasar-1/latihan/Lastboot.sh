07433ea4cc624b963524ee6f7d265cbd4a0316b8
Wizfie <111037756+Wizfie@users.noreply.github.com>
2022-08-11 20:44:44 +0700

    Delete Lastboot.sh

--- Kemampuan-dasar/kemampuan-dasar-1/latihan/Lastboot.sh
+++
@@ -1,8 +0,0 @@
-PS C:\WINDOWS\system32> C:\Get-LastBootTime.ps1
-cmdlet Get-LastBootTime.ps1 at command pipeline position 1
-Supply values for the following parameters:
-ComputerName: Home
-
-CSName Last Booted        
------- -----------        
-HOME   11/08/2022 20:07:41
