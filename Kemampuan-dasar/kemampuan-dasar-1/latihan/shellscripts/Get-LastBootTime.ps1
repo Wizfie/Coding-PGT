783456030c39907d1be8166b5f4337eddf627d02
Wizfie <111037756+Wizfie@users.noreply.github.com>
2022-08-12 10:19:56 +0700

    Add files via upload

--- Kemampuan-dasar/kemampuan-dasar-1/latihan/shellscripts/Get-LastBootTime.ps1
+++ Kemampuan-dasar/kemampuan-dasar-1/latihan/shellscripts/Get-LastBootTime.ps1
@@ -1,8 +1,19 @@
-PS C:\WINDOWS\system32> C:\Get-LastBootTime.ps1
-cmdlet Get-LastBootTime.ps1 at command pipeline position 1
-Supply values for the following parameters:
-ComputerName: Home
+﻿<#
+.SYNOPSIS
+Shows when last your PC started up.
+.DESCRIPTION
+This is a WMI wrapper function to get the time that your PC last started up.
+.PARAMETER ComputerName
+The name of the Computer you want to run the command against.
+.EXAMPLE
+Get-LastBootTime -ComputerName localhost
+.LINK
+www.howtogeek.com
+#>
+param(
+    [Parameter(Mandatory=$true)][string]$ComputerName
+)
 
-CSName Last Booted        
------- -----------        
-HOME   11/08/2022 20:07:41
+Get-WmiObject -Class Win32_OperatingSystem –ComputerName $ComputerName |
+Select-Object -Property CSName,@{n=”Last Booted”;
+e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
