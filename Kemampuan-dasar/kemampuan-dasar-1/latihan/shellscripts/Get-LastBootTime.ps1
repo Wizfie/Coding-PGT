50912001b7246ac539dd10e8c2e117cf3c393d83
Wizfie <111037756+Wizfie@users.noreply.github.com>
2022-08-12 10:20:45 +0700

    Update Get-LastBootTime.ps1

--- Kemampuan-dasar/kemampuan-dasar-1/latihan/shellscripts/Get-LastBootTime.ps1
+++ Kemampuan-dasar/kemampuan-dasar-1/latihan/shellscripts/Get-LastBootTime.ps1
@@ -17,3 +17,14 @@ param(
 Get-WmiObject -Class Win32_OperatingSystem –ComputerName $ComputerName |
 Select-Object -Property CSName,@{n=”Last Booted”;
 e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
+
+==============================================================================================================================
+Hasil :
+PS C:\WINDOWS\system32> C:\Get-LastBootTime.ps1
+cmdlet Get-LastBootTime.ps1 at command pipeline position 1
+Supply values for the following parameters:
+ComputerName: Home
+
+CSName Last Booted        
+------ -----------        
+HOME   11/08/2022 20:07:41
