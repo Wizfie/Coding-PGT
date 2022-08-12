ce514c8f49b8d1c890d1488dd5961aa8f642fc9f
Wizfie <111037756+Wizfie@users.noreply.github.com>
2022-08-12 10:34:09 +0700

    Update FirstISEScript.ps1

--- Kemampuan-dasar/kemampuan-dasar-1/latihan/shellscripts/FirstISEScript.ps1
+++ Kemampuan-dasar/kemampuan-dasar-1/latihan/shellscripts/FirstISEScript.ps1
@@ -1,2 +1,10 @@
 ﻿Get-WmiObject -Class Win32_OperatingSystem –ComputerName localhost |
 Select-Object -Property CSName,FreeVirtualMemory 
+
+==========================================================================================================================================================================
+# Hasil
+PS C:\WINDOWS\system32> C:\Scripts\Artikel 3\FirstISEScript.ps1
+
+CSName FreeVirtualMemory
+------ -----------------
+HOME             9727252
